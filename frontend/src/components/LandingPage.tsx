ea9baff5ad70a68ff7ea55711c59a34f8ca2d850
FraFle68 <frank.fleischer@wolfsburg.de>
2023-10-19 23:33:29 +0200

    finished landingpage 0.2

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,29 +1,30 @@
 export default function LandingPage() {
     return (
-        <>
-            <div className="landingPageGreeting">
-                <h2>Potion of Power</h2>
-                <p>
-                    The known world ist in trouble. Orks are attacking the free races,
-                    Elves and dwarves are fighting each other and the nature is training for
-                    the apocalypse.
-                </p>
-                <p>
-                    There seems to be nobody who is able to stop the chaos.</p>
-                <p>
-                    But there is hope.
-                    Rumors tell about a potion of power which is hidden in the dungeon of an ancient
-                    wizard. The person drinking this potion will be able to save the world.
-                </p>
-                <p>
-                    You stand in front of this dungeon and your will is to get
-                    this potion whatever it may cost you.
-                </p>
-
-
-
+        <div className="landingPage">
+            <div className="landingPageGreetingFrame">
+                <div className="landingPageGreeting">
+                    <h1>Potion of Power</h1>
+                    <p>
+                        The known world is in trouble. Orcs are attacking the free races,
+                        elves and dwarves are fighting each other and nature is preparing
+                        for the apocalypse.
+                    </p>
+                    <p>
+                        There seems to be nobody who is able to stop the chaos.</p>
+                    <p>
+                        But there is hope. Rumors are circulating about a potion
+                        of power hidden in the dungeon of an ancient wizard. They promise that he
+                        person who drinks this potion will be able to save the world.
+                    </p>
+                    <p>
+                        You stand in front of this dungeon, determined to obtain this
+                        potion, whatever the cost may be.
+                    </p>
+                </div>
             </div>
-            <button className="greenButton">Log in</button>
-        </>
+            <div className="landingPageButton">
+                <button className="greenButton">Log in</button>
+            </div>
+        </div>
     )
 }
