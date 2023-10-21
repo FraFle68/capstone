841a41147232ecb095cb25287e0f49084760b917
FraFle68 <frank.fleischer@wolfsburg.de>
2023-10-21 23:02:03 +0200

    characterPage 0.2

--- frontend/src/components/CharacterPage.tsx
+++ frontend/src/components/CharacterPage.tsx
@@ -13,16 +13,30 @@ export default function CharacterPage() {
                     <div>
                         <h2>Adventurer Licence</h2>
                     </div>
-                    <div>
-                        <div>
-                            <img alt="" typeof="image/png" src={imageToAdd}/>
-                            Gandalf
-                            <input type="radio" defaultChecked={false} name="job" value="warrior"/>Warrior
-                            <input type="radio" defaultChecked={true} name="job" value="mage"/>Mage
+
+                    <div className="cardBoxRow">
+                        <img alt="" typeof="image/png" src={imageToAdd}/>
+                        <div className="cardBoxColumn">
+                            <div>Name: <input type="text" name="name" value="Gandalf"/></div>
+                            <div>Titel: Nobody</div>
                         </div>
+                        <div className="cardBoxColumn">
+                            <div><input type="radio" defaultChecked={false} name="job" value="warrior"/>Warrior</div>
+                            <div><input type="radio" defaultChecked={true} name="job" value="mage"/>Mage</div>
+                        </div>
+
                     </div>
-                    <div>
-                        There seems to be nobody who is able to stop the chaos.
+                    <div className="cardBoxRow">
+                        <div>
+                            Level: 1<br/>
+                            Strength: 5<br/>
+                            Intelligence: 7
+                        </div>
+                        <div>
+                            Experience: 0<br/>
+                            Weapon: Sword<br/>
+                            Armor: Cloth
+                        </div>
                     </div>
                     <div style={{textAlign: "end"}}><button>save</button></div>
 
