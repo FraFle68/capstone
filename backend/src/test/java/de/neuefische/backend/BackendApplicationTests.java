e3ad9b0a4ce91f4e172f7f7daefe47e5bf9e8155
FraFle68 <frank.fleischer@wolfsburg.de>
2023-11-07 15:04:45 +0100

    fixed add getItem

--- backend/src/test/java/de/neuefische/backend/BackendApplicationTests.java
+++ backend/src/test/java/de/neuefische/backend/BackendApplicationTests.java
@@ -2,21 +2,18 @@ package de.neuefische.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @MockBean
+    private ClientRegistrationRepository clientRegistrationRepository;
 
-    @DynamicPropertySource
-    static void setUrlDynamically(DynamicPropertyRegistry reg) {
-        reg.add("app.openai-api-key", ()->"dummy_api_key");
-        reg.add("app.openai-api-org", ()->"dummy_api_org");
-        reg.add("app.openai-api-url", ()->"dummy_url");
-    }
+
 
     @Test
     void contextLoads() {
