594ae8e1f74b089194da5f8d0d45748f22750e38
FraFle68 <frank.fleischer@wolfsburg.de>
2023-11-09 11:10:49 +0100

    add some tests

--- backend/src/test/java/de/neuefische/backend/service/GameServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/GameServiceTest.java
@@ -1,70 +1,104 @@
 package de.neuefische.backend.service;
 
-import de.neuefische.backend.game.core.Room;
 import de.neuefische.backend.game.core.Vector2d;
 import de.neuefische.backend.game.dungeon.Dungeon;
-import de.neuefische.backend.game.dungeon.DungeonCreator;
 import de.neuefische.backend.model.GameMap;
 import de.neuefische.backend.repository.GameRepository;
 import de.neuefische.backend.repository.RoomRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class GameServiceTest {
-    private final GameRepository mockGameRepo = mock(GameRepository.class);
-    private final RoomRepository mockRoomRepo = mock(RoomRepository.class);
-    final DungeonCreator mockDungeonCreator = mock(DungeonCreator.class);
 
+    @Mock
+    private GameRepository gameRepository;
 
-    GameService gameService = new GameService(mockGameRepo, mockRoomRepo);
+    @Mock
+    private RoomRepository roomRepository;
 
-    /*@Test
-    void testGetMap_withMapInDatabase() {
+    @InjectMocks
+    private GameService gameService;
 
-        GameMap expectedMap = new GameMap("123", new Dungeon(), new Vector2d(0, 0));
-        when(mockGameRepo.findById("123")).thenReturn(Optional.of(expectedMap));
-
-        GameMap result = gameService.getMap("123");
-        //THEN
-        verify(mockGameRepo).findById("123");
-
-        assertEquals(expectedMap, result);
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void testGetMap_withoutMapInDatabase() {
+    void testGetMapExistingMap() {
+        String gameId = "1";
+        GameMap expectedMap = new GameMap(gameId, new Dungeon(50, 50, 0), new Vector2d(0, 0));
 
-        List<Room> rooms = new ArrayList<>();
-        rooms.add(new Room(1, 5, 5));
+        when(gameRepository.findById(gameId)).thenReturn(Optional.of(expectedMap));
 
-        GameMap expectedMap = new GameMap("123", new Dungeon(), new Vector2d(0, 0));
-        when(mockGameRepo.findById("123")).thenReturn(null);
-        when(mockDungeonCreator.createDungeon(50, 50, 0, rooms)).thenReturn(new Dungeon());
+        GameMap retrievedMap = gameService.getMap(gameId);
 
-        GameMap result = gameService.getMap("123");
-        //THEN
-        verify(mockGameRepo).findById("123");
+        assertEquals(expectedMap, retrievedMap);
+        verify(gameRepository, times(1)).findById(gameId);
+        verify(gameRepository, times(1)).save(expectedMap);
+    }
 
-        assertEquals(expectedMap, result);
+    /*@Test
+    void testGetMapNewMap() {
+        String gameId = "2";
+
+        when(gameRepository.findById(gameId)).thenReturn(Optional.empty());
+        when(roomRepository.findAll()).thenReturn(new ArrayList<>());
+
+        GameMap retrievedMap = gameService.getMap(gameId);
+
+        assertEquals(gameId, retrievedMap.id());
+        verify(gameRepository, times(1)).findById(gameId);
+        verify(gameRepository, times(1)).save(any(GameMap.class));
     }*/
 
     @Test
     void testDeleteMap() {
-        //GIVEN
-        String id = "123";
+        String gameId = "1";
 
-        //WHEN
-        gameService.deleteMap(id);
+        gameService.deleteMap(gameId);
 
-        //THEN
-        verify(mockGameRepo).deleteById(id);
+        verify(gameRepository, times(1)).deleteById(gameId);
     }
 
+    @Test
+    void testChangePosition() {
+        String gameId = "1";
+        Vector2d newPosition = new Vector2d(1, 1);
+        GameMap existingMap = new GameMap(gameId, new Dungeon(50, 50, 0), new Vector2d(1, 1));
 
+        when(gameRepository.findById(gameId)).thenReturn(Optional.of(existingMap));
+        when(gameRepository.save(any(GameMap.class))).thenReturn(existingMap);
+
+        GameMap updatedMap = gameService.changePosition(gameId, newPosition);
+
+        assertEquals(newPosition, updatedMap.position());
+        verify(gameRepository, times(1)).findById(gameId);
+        verify(gameRepository, times(1)).save(any(GameMap.class));
+    }
+
+    @Test
+    void testGetItem() {
+        String gameId = "1";
+        Vector2d position = new Vector2d(1, 1);
+        GameMap existingMap = new GameMap(gameId, new Dungeon(50, 50, 0), new Vector2d(0, 0));
+
+        when(gameRepository.findById(gameId)).thenReturn(Optional.of(existingMap));
+        when(gameRepository.save(any(GameMap.class))).thenReturn(existingMap);
+
+        GameMap updatedMap = gameService.getItem(gameId, position);
+
+        assertEquals(0, updatedMap.dungeon().getContentMap()[position.getY()][position.getX()]);
+        verify(gameRepository, times(1)).findById(gameId);
+        verify(gameRepository, times(1)).save(any(GameMap.class));
+    }
 }
